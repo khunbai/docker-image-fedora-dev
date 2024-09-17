505dd820f89eb7f44b21f6c5b39beefa2895569d
theerapat-s28 <theerapat.s28@gmail.com>
2024-09-17 16:20:00 +0000

    [main][s] add vertical scroll keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,13 +6,18 @@
 -- vim.keymap.del('i', '<C-[>'); -- To be used for copilot panel
 -- vim.keymap.del('i', '<C-]>'); -- To be used for copilot panel
 
-local map = LazyVim.safe_keymap_set;
+local map = vim.keymap.set;
+local del = vim.keymap.del;
 
 map('n', 'U', '<C-r>', { desc = "Redo" });
 map('x', "p", '"_dP', { desc = "Paste without yanking" });
 map('n', 'gh', '_', { desc = "Go to first non-blank character of the line" });
 map('v', 'gh', '_', { desc = "Go to first non-blank character of the line" });
-map('n', 'gl', '$', { desc = "Go to end of line" });
+map('n', 'gl', '$', { desc = "Go to end glf line" });
 map('v', 'gl', '$', { desc = "Go to end of line" });
+map({ 'n', 'v' }, 'gj', '<C-d>', { desc = "Scroll down" });
+map({ 'n', 'v' }, 'gk', '<C-u>', { desc = "Scroll up" });
 map({ 'i', 'v' }, '<A-i>', '<Esc>', { desc = "Exit insert mode" });
 map({ 'n', 'v' }, 'x', 'xi', { desc = "Delete a character and go to insert mode" });
+map('n', '<leader>qh', '<CMD>q!<CR>', { desc = "Quit current file without save." });
+map('n', '<leader>qw', '<CMD>wq<CR>', { desc = "Save current file and quit." });
